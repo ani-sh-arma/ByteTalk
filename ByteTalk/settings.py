34c4f469e0067f6e31ad3ec01ec3683dd91a09d9
Anish <anisharma030@gmail.com>
2024-01-13 19:20:54 +0530

    added requirements.txt and vercel.json

--- ByteTalk/settings.py
+++ ByteTalk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^$ppdpp5-+03pc4sw$sv)4%!qh=s1s(6(qc$__&059n--uhl%g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
