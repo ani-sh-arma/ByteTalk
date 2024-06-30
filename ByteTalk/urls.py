7430c3047480085b35425b0a8c7949708701e153
Anish <anisharma030@gmail.com>
2024-06-30 22:21:41 +0530

    vercel.json changes

--- ByteTalk/urls.py
+++ ByteTalk/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path , include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('home.urls')),
 
     path("__reload__/", include("django_browser_reload.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
